3f764f4100c46cb6051f1c0d4501285edd497ab4
Ashish Desai <desaiashish212@gmail.com>
2016-06-04 12:24:06 +0530

    notification sending done
    send notification to all users

--- worker_notifications.php
+++ worker_notifications.php
@@ -8,11 +8,34 @@
 	$sql = "insert into notifications (messages,language) values ('$message','$languages')";
 	//$aff=mysql_affected_rows();
 	$result = mysqli_query($con, $sql);
-	
+	$pushStatus = '';
 	if($sql)
    {
-			$_SESSION["MSG"]="Success";
+	  $query = "SELECT gcm_reg_id FROM users WHERE lang_status=1";
+    if($query_run = mysqli_query($con,$query)) {
+
+        $gcmRegIds = array();
+        while($query_row = mysqli_fetch_assoc($query_run)) {
+
+            array_push($gcmRegIds, $query_row['gcm_reg_id']);
+
+        }
+
+    }
+    $pushMessage = $_POST['message'];
+    if(isset($gcmRegIds) && isset($pushMessage)) {
+
+        $message = array('message' => $pushMessage);
+        $pushStatus = sendPushNotification($gcmRegIds, $message);
+		echo $pushStatus;
+
+		exit;
+		$_SESSION["MSG"]="Success";
 			header("Location: notifications.php");
+
+    }   
+//	}
+			
    }
    else
    {
@@ -21,4 +44,34 @@
 			header("Location: notifications.php");
    		
    }
+  
+   
+   function sendPushNotification($registatoin_ids, $message) {
+		//Google cloud messaging GCM-API url
+        $url = 'https://android.googleapis.com/gcm/send';
+        $fields = array(
+            'registration_ids' => $registatoin_ids,
+            'data' => $message,
+        );
+		// Update your Google Cloud Messaging API Key
+		define("GOOGLE_API_KEY", "AIzaSyDqa_hGc5tMKTVSmrZhwO2lmiGYZCUyNtc"); 		
+        $headers = array(
+            'Authorization: key=' . GOOGLE_API_KEY,
+            'Content-Type: application/json'
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		//curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);	
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);				
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }
+        curl_close($ch);
+        return $result;
+    }
 ?>
