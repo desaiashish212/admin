543af1fe2353c7e96a6682003ae9a4575df7882c
Ashish Desai <desaiashish212@gmail.com>
2016-07-15 15:28:10 +0530

    services update

--- services/getCity.php
+++ services/getCity.php
@@ -1,18 +1,39 @@
 <?php
-$China = array("Beijing","Shanghai","Hong Kong","Xichang");
-$India = array('Chennai','Delhi','Kolkata','Mumbai');
-$UK = array('Cambridge','Derby','Liverpool','Oxford');
-$US = array('Los Angeles','New York','Chicago','San Diego');
-$country = $_GET["district"];
-if($country == "Mumbai"){
-$cityJSON = json_encode($India);
-}else if($country == "Chennai"){
-$cityJSON = json_encode($China);
-}else if($country == "Delhi"){
-$cityJSON = json_encode($UK);
-}else if($country == "Beijing"){
-$cityJSON = json_encode($US);
+include_once ('connection.php');
+
+if (isset($_GET['district'])) {
+	
+	$district = $_GET["district"];
+	$districtid;
+	
+	$res = mysqli_query($con,"SELECT dist.distid FROM dist WHERE dist.distname = '$district'") or die(mysql_error());
+	if($res){
+		while ($row = mysqli_fetch_array($res)) {
+			$districtid = $row['distid'];
+		}
+		
+	
+		$result = mysqli_query($con,"SELECT city.cityname FROM city WHERE city.distid = '$districtid' ") or die(mysql_error());
+	
+	if($result){
+		$city = array();
+		while ($row = mysqli_fetch_array($result)) {
+			array_push($city, $row["cityname"]);
+		}
+		
+		$cityJSON = json_encode($city);
+		echo "{Cities=".$cityJSON."}";
+		
+	}else{
+		$response["message"] = "No City found";
+	}
+	}else{
+		$response["message"] = "No City found";
+	}	
+} else {
+    
+    $response["message"] = "Required field(s) is missing";
+
+    echo json_encode($response);
 }
- 
-echo "{Cities=".$cityJSON."}";
 ?>
---
+++ services/getDist.php
@@ -0,0 +1,29 @@
+<?php
+include_once ('connection.php');
+
+if (isset($_GET['state'])) {
+	
+	$state = $_GET["state"];
+	
+	$result = mysqli_query($con,"SELECT dist.distname FROM dist WHERE dist.stateid = '$state' ") or die(mysql_error());
+	
+	if($result){
+		$dist = array();
+		while ($row = mysqli_fetch_array($result)) {
+			array_push($dist, $row["distname"]);
+		}
+		
+		$distJSON = json_encode($dist);
+		echo "{Cities=".$distJSON."}";
+		
+	}else{
+		$response["message"] = "No District found";
+	}
+	district
+} else {
+    
+    $response["message"] = "Required field(s) is missing";
+
+    echo json_encode($response);
+}
+?>
